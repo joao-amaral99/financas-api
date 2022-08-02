d63aa4c571263b8e89d31a56455365af6a383e82
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-01 22:02:46 -0300

    feat: import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
