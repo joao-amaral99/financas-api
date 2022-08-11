2040b2d78449527ca72fea02f5734184e18baafc
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-10 22:57:21 -0300

    feat: import category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [UserModule, AuthModule, ConfigModule.forRoot()],
+  imports: [UserModule, AuthModule, ConfigModule.forRoot(), CategoryModule],
   controllers: [],
   providers: [],
 })
