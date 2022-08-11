ed811798de015dc06048e211c5824de325f009ac
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-10 22:55:45 -0300

    feat: create category

---
+++ src/category/category.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Controller('api/v1/category')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  async findAll() {
+    return await this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.categoryService.findOne({ id: +id });
+  }
+
+  @Post()
+  async create(@Body() createCategoryDto: CreateCategoryDto) {
+    return await this.categoryService.create(createCategoryDto);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return await this.categoryService.update(+id, updateCategoryDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return await this.categoryService.remove({ id: +id });
+  }
+}
---
+++ src/category/category.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma, Category } from '@prisma/client';
+
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Injectable()
+export class CategoryService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(): Promise<Category[]> {
+    return await this.prisma.category.findMany();
+  }
+
+  async findOne(where: Prisma.UserWhereUniqueInput): Promise<Category> {
+    try {
+      return await this.prisma.category.findUnique({
+        where,
+      });
+    } catch (error) {
+      throw new InternalServerErrorException('Id informado não existe');
+    }
+  }
+
+  async create(data: CreateCategoryDto): Promise<Category> {
+    try {
+      return await this.prisma.category.create({
+        data: {
+          name: data.name,
+          expensesId: data.expensesId,
+        },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Já existe categoria com esse nome',
+      );
+    }
+  }
+
+  async update(id: number, data: UpdateCategoryDto): Promise<Category> {
+    try {
+      const user = await this.findOne({ id });
+
+      if (user) {
+        return await this.prisma.category.update({
+          where: { id },
+          data,
+        });
+      }
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Opa, algo deu errado, tente novamente',
+      );
+    }
+  }
+
+  async remove(where: Prisma.UserWhereUniqueInput): Promise<Category> {
+    try {
+      return await this.prisma.category.delete({
+        where,
+      });
+    } catch (error) {
+      throw new InternalServerErrorException('Id informado não existe');
+    }
+  }
+}
