93ee00813d1738ff89616bfd0eb7b00259bb2ce5
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-11 22:40:28 -0300

    refactor: remove unused properties

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,11 +1,7 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty()
   name: string;
-
-  @IsNumber()
-  @IsNotEmpty()
-  expensesId: number;
 }
