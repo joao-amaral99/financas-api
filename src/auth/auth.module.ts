b8e33db4f41dee42f274b00fbfa784686d7a2dfe
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-23 23:41:13 -0300

    feat: import token module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,12 +7,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { TokenModule } from 'src/token/token.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     PassportModule,
     UserModule,
+    TokenModule,
     JwtModule.register({
       privateKey: process.env.SECRET_KEY,
       signOptions: { expiresIn: '360s' },
