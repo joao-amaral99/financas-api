c7d4d69cc5d5290fa2ba3a03d6886646bc206cad
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-24 23:31:10 -0300

    feat: add method refresh token

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,28 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import {
+  forwardRef,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateTokenDto } from './dto/create-token.dto';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from 'src/auth/auth.service';
 
 @Injectable()
 export class TokenService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly userService: UserService,
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
 
   async save(hash: string, email: string) {
-    const userToken = await this.prismaService.token.findUnique({
+    const userToken = await this.prisma.token.findUnique({
       where: { email: email },
     });
 
     if (userToken) {
-      await this.prismaService.token.update({
+      await this.prisma.token.update({
         where: { email: userToken.email },
         data: { hash: hash },
       });
     }
 
-    await this.prismaService.token.create({
+    this.prisma.token.create({
       data: {
         hash,
         email,
       },
     });
+  }
+
+  async refreshToken(oldToken: string) {
+    const token = await this.prisma.token.findFirst({
+      where: { hash: oldToken },
+    });
+
+    if (token) {
+      const user = await this.userService.findOne({ email: token.email });
+      return this.authService.login(user);
+    }
+
+    return new HttpException(
+      {
+        message: 'Token inválido',
+      },
+      HttpStatus.UNAUTHORIZED,
+    );
   }
 }
