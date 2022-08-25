31ed5a90b5d4fcd83ef3d762c47e808c0953d25a
João Amaral <joao.amaral.almeida99@gmail.com>
2022-08-24 23:30:45 -0300

    fix: circular dependences

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { TokenController } from './token.controller';
+import { UserModule } from 'src/user/user.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  controllers: [],
+  imports: [forwardRef(() => AuthModule), UserModule],
+  controllers: [TokenController],
   providers: [TokenService, PrismaService],
   exports: [TokenService],
 })
